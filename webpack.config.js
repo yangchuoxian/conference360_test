4a9355c970402f18a587f30957807e22f218647c
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2016-06-09 07:10:51 +0800

    Fixed moment dependency issue

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,17 @@ module.exports = {
 		path: __dirname,
 		filename: './dist/bundle.js',
 	},
+	watch: false,
 	resolve: {
 		extensions: ['', '.js', '.ts']
 	},
+	// Depending on whether PROD_ENV=1 or not when running PROD_ENV=1 webpack, the following setting uses compress webpack plugin to minify the bundled js file
 	plugins: PROD ? [
 		new webpack.optimize.UglifyJsPlugin({
 			compress: { warnings: false }
 		})
 	] : [],
+	// load typescript source files with ts-loade and compiles
 	module: {
 		loaders: [{
 			test: /\.ts/, loaders: ['ts-loader'], exclude: /node_modules/
